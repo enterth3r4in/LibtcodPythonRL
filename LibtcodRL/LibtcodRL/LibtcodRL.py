f40a100b9faba565fde4d466c0802abc2d069764
enterth3r4in <believe.the.rain@gmail.com>
2015-10-27 13:06:47 -0400

    Startup and initial files needed for libtcod

---
+++ LibtcodRL/LibtcodRL/LibtcodRL.py
@@ -0,0 +1,16 @@
+﻿import libtcodpy as ltc
+
+WIDTH = 800
+HEIGHT = 600
+TITLE = "Libtcod RL"
+
+ltc.console_init_root(WIDTH, HEIGHT, TITLE)
+console = ltc.console_new(WIDTH, HEIGHT)
+ltc.console_blit(console, 0, 0, WIDTH, HEIGHT, 0, 0, 0)
+
+# MAIN GAME LOOP
+
+ltc.console_flush()
+ltc.console_fill_background(console, 100, 100, 100)
+ltc.console_blit(console, 0, 0, WIDTH, HEIGHT, 0, 0, 0)
+ltc.console_clear(console)
